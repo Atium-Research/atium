f900ed405d1ff2a68f86844484bdb71ff9c90bfe
Andrew Hall <andrewmartinhall2@gmail.com>
2026-02-14 13:16:20 -0700

    Reset

---
+++ test.py
@@ -0,0 +1,24 @@
+import bear_lake as bl
+import os
+
+access_key_id = os.getenv("ACCESS_KEY_ID")
+secret_access_key = os.getenv("SECRET_ACCESS_KEY")
+region = os.getenv("REGION")
+endpoint = os.getenv("ENDPOINT")
+bucket = os.getenv("BUCKET")
+
+
+storage_options = {
+    "aws_access_key_id": access_key_id,
+    "aws_secret_access_key": secret_access_key,
+    "region": region,
+    "endpoint_url": endpoint,
+}
+
+url = f"s3://{bucket}"
+
+db = bl.connect_s3(path=url, storage_options=storage_options)
+
+print(
+    db.query(bl.table("signals"))
+)
